76c218f0ceb494c1adcd7e70d600dfd736c628c2
Morgan Christiansson <git@mog.se>
2026-03-15 21:22:54 +0100

    Dead code has been moved

--- code/matrix_launcher/src/ltlogin/loginmediator.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Transitional compatibility wrapper.
-// Canonical launcher-login implementation now lives under the recovered source-style path:
-// - matrixstaging/game/src/libltclientlogin/loginmediator.h
-
-#include "../../matrixstaging/game/src/libltclientlogin/loginmediator.h"
--- code/matrix_launcher/src/ltlogin/loginstates.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Transitional compatibility wrapper.
-// Canonical launcher-login-state implementation now lives under the recovered source-style path:
-// - matrixstaging/game/src/libltclientlogin/loginstate.h
-
-#include "../../matrixstaging/game/src/libltclientlogin/loginstate.h"
