9ad721a149c8e82c5f26920d162539c0da016f27
Morgan Christiansson <git@mog.se>
2026-04-25 18:37:51 +0200

    Debug: add logging to trace State9 submit flow
    - Add handoffWord09 parse debug logging to trace what's parsed from server
    - Add State9 slot3 debug logging to trace pending payload values
    
    This revealed test server sends garbage (0x465c/0xc0cc/0x001b) at offset +9
    instead of the expected handoff port 0x2710 (10000). Our port override
    fix works correctly - ignores garbage and uses marginConnection port.

---
+++ code/matrix_launcher/matrixstaging/game/src/libltclientlogin/loginmediator_state9.d
@@ -0,0 +1,190 @@
+matrixstaging/game/src/libltclientlogin/loginmediator_state9.o: \
+ matrixstaging/game/src/libltclientlogin/loginmediator_state9.cpp \
+ matrixstaging/game/src/libltclientlogin/loginmediator.h \
+ matrixstaging/game/src/libltclientlogin/../../../runtime/src/libltcrypto/auth_crypto.h \
+ matrixstaging/game/src/libltclientlogin/../../../runtime/src/libltmessaging/messageconnection.h \
+ third_party/spdlog-1.17.0/include/spdlog/spdlog.h \
+ third_party/spdlog-1.17.0/include/spdlog/common.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/null_mutex.h \
+ third_party/spdlog-1.17.0/include/spdlog/tweakme.h \
+ third_party/spdlog-1.17.0/include/spdlog/fmt/fmt.h \
+ third_party/spdlog-1.17.0/include/spdlog/fmt/bundled/format.h \
+ third_party/spdlog-1.17.0/include/spdlog/fmt/bundled/base.h \
+ third_party/spdlog-1.17.0/include/spdlog/fmt/bundled/format.h \
+ third_party/spdlog-1.17.0/include/spdlog/fmt/bundled/format-inl.h \
+ third_party/spdlog-1.17.0/include/spdlog/common-inl.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/registry.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/periodic_worker.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/periodic_worker-inl.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/registry-inl.h \
+ third_party/spdlog-1.17.0/include/spdlog/logger.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/backtracer.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/circular_q.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/log_msg_buffer.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/log_msg.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/log_msg-inl.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/os.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/os-inl.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/windows_include.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/log_msg_buffer-inl.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/backtracer-inl.h \
+ third_party/spdlog-1.17.0/include/spdlog/logger-inl.h \
+ third_party/spdlog-1.17.0/include/spdlog/pattern_formatter.h \
+ third_party/spdlog-1.17.0/include/spdlog/formatter.h \
+ third_party/spdlog-1.17.0/include/spdlog/pattern_formatter-inl.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/fmt_helper.h \
+ third_party/spdlog-1.17.0/include/spdlog/mdc.h \
+ third_party/spdlog-1.17.0/include/spdlog/sinks/sink.h \
+ third_party/spdlog-1.17.0/include/spdlog/sinks/sink-inl.h \
+ third_party/spdlog-1.17.0/include/spdlog/sinks/wincolor_sink.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/console_globals.h \
+ third_party/spdlog-1.17.0/include/spdlog/sinks/wincolor_sink-inl.h \
+ third_party/spdlog-1.17.0/include/spdlog/details/synchronous_factory.h \
+ third_party/spdlog-1.17.0/include/spdlog/version.h \
+ third_party/spdlog-1.17.0/include/spdlog/spdlog-inl.h \
+ matrixstaging/game/src/libltclientlogin/../../../runtime/src/libltmessaging/../libltcrypto/auth_internal.h \
+ third_party/cryptopp890/base64.h third_party/cryptopp890/cryptlib.h \
+ third_party/cryptopp890/config.h third_party/cryptopp890/config_align.h \
+ third_party/cryptopp890/config_asm.h third_party/cryptopp890/config_os.h \
+ third_party/cryptopp890/config_ver.h \
+ third_party/cryptopp890/config_cpu.h \
+ third_party/cryptopp890/config_cxx.h \
+ third_party/cryptopp890/config_dll.h \
+ third_party/cryptopp890/config_int.h third_party/cryptopp890/config_ns.h \
+ third_party/cryptopp890/config_misc.h third_party/cryptopp890/stdcpp.h \
+ third_party/cryptopp890/trap.h third_party/cryptopp890/basecode.h \
+ third_party/cryptopp890/filters.h third_party/cryptopp890/simple.h \
+ third_party/cryptopp890/misc.h third_party/cryptopp890/secblockfwd.h \
+ third_party/cryptopp890/smartptr.h third_party/cryptopp890/secblock.h \
+ third_party/cryptopp890/allocate.h third_party/cryptopp890/queue.h \
+ third_party/cryptopp890/algparam.h third_party/cryptopp890/integer.h \
+ third_party/cryptopp890/argnames.h third_party/cryptopp890/crc.h \
+ third_party/cryptopp890/filters.h third_party/cryptopp890/md5.h \
+ third_party/cryptopp890/iterhash.h third_party/cryptopp890/modes.h \
+ third_party/cryptopp890/strciphr.h third_party/cryptopp890/seckey.h \
+ third_party/cryptopp890/osrng.h third_party/cryptopp890/randpool.h \
+ third_party/cryptopp890/aes.h third_party/cryptopp890/rijndael.h \
+ third_party/cryptopp890/fips140.h third_party/cryptopp890/hkdf.h \
+ third_party/cryptopp890/hmac.h third_party/cryptopp890/rng.h \
+ third_party/cryptopp890/sha.h third_party/cryptopp890/oaep.h \
+ third_party/cryptopp890/pubkey.h third_party/cryptopp890/algebra.h \
+ third_party/cryptopp890/modarith.h third_party/cryptopp890/eprecomp.h \
+ third_party/cryptopp890/rsa.h third_party/cryptopp890/pkcspad.h \
+ third_party/cryptopp890/hashfwd.h third_party/cryptopp890/oaep.h \
+ third_party/cryptopp890/emsa2.h third_party/cryptopp890/asn.h \
+ third_party/cryptopp890/sha.h third_party/cryptopp890/twofish.h \
+ matrixstaging/game/src/libltclientlogin/../../../runtime/src/libltmessaging/../liblttcp/ltthreadperclienttcpengine.h \
+ matrixstaging/game/src/libltclientlogin/../../../runtime/src/libltmessaging/../liblttcp/ilttcpengine.h \
+ matrixstaging/game/src/libltclientlogin/../../../runtime/src/libltmessaging/../liblttcp/lttcpconnection.h \
+ matrixstaging/game/src/libltclientlogin/../../../runtime/src/liblttcp/ltipaddresslist.h \
+ matrixstaging/game/src/libltclientlogin/loginmediator_base.h \
+ matrixstaging/game/src/libltclientlogin/authbootstrap680.h \
+ matrixstaging/game/src/libltclientlogin/loginmediator_state9_submit.h \
+ matrixstaging/game/src/libltclientlogin/loginstate.h
+matrixstaging/game/src/libltclientlogin/loginmediator.h:
+matrixstaging/game/src/libltclientlogin/../../../runtime/src/libltcrypto/auth_crypto.h:
+matrixstaging/game/src/libltclientlogin/../../../runtime/src/libltmessaging/messageconnection.h:
+third_party/spdlog-1.17.0/include/spdlog/spdlog.h:
+third_party/spdlog-1.17.0/include/spdlog/common.h:
+third_party/spdlog-1.17.0/include/spdlog/details/null_mutex.h:
+third_party/spdlog-1.17.0/include/spdlog/tweakme.h:
+third_party/spdlog-1.17.0/include/spdlog/fmt/fmt.h:
+third_party/spdlog-1.17.0/include/spdlog/fmt/bundled/format.h:
+third_party/spdlog-1.17.0/include/spdlog/fmt/bundled/base.h:
+third_party/spdlog-1.17.0/include/spdlog/fmt/bundled/format.h:
+third_party/spdlog-1.17.0/include/spdlog/fmt/bundled/format-inl.h:
+third_party/spdlog-1.17.0/include/spdlog/common-inl.h:
+third_party/spdlog-1.17.0/include/spdlog/details/registry.h:
+third_party/spdlog-1.17.0/include/spdlog/details/periodic_worker.h:
+third_party/spdlog-1.17.0/include/spdlog/details/periodic_worker-inl.h:
+third_party/spdlog-1.17.0/include/spdlog/details/registry-inl.h:
+third_party/spdlog-1.17.0/include/spdlog/logger.h:
+third_party/spdlog-1.17.0/include/spdlog/details/backtracer.h:
+third_party/spdlog-1.17.0/include/spdlog/details/circular_q.h:
+third_party/spdlog-1.17.0/include/spdlog/details/log_msg_buffer.h:
+third_party/spdlog-1.17.0/include/spdlog/details/log_msg.h:
+third_party/spdlog-1.17.0/include/spdlog/details/log_msg-inl.h:
+third_party/spdlog-1.17.0/include/spdlog/details/os.h:
+third_party/spdlog-1.17.0/include/spdlog/details/os-inl.h:
+third_party/spdlog-1.17.0/include/spdlog/details/windows_include.h:
+third_party/spdlog-1.17.0/include/spdlog/details/log_msg_buffer-inl.h:
+third_party/spdlog-1.17.0/include/spdlog/details/backtracer-inl.h:
+third_party/spdlog-1.17.0/include/spdlog/logger-inl.h:
+third_party/spdlog-1.17.0/include/spdlog/pattern_formatter.h:
+third_party/spdlog-1.17.0/include/spdlog/formatter.h:
+third_party/spdlog-1.17.0/include/spdlog/pattern_formatter-inl.h:
+third_party/spdlog-1.17.0/include/spdlog/details/fmt_helper.h:
+third_party/spdlog-1.17.0/include/spdlog/mdc.h:
+third_party/spdlog-1.17.0/include/spdlog/sinks/sink.h:
+third_party/spdlog-1.17.0/include/spdlog/sinks/sink-inl.h:
+third_party/spdlog-1.17.0/include/spdlog/sinks/wincolor_sink.h:
+third_party/spdlog-1.17.0/include/spdlog/details/console_globals.h:
+third_party/spdlog-1.17.0/include/spdlog/sinks/wincolor_sink-inl.h:
+third_party/spdlog-1.17.0/include/spdlog/details/synchronous_factory.h:
+third_party/spdlog-1.17.0/include/spdlog/version.h:
+third_party/spdlog-1.17.0/include/spdlog/spdlog-inl.h:
+matrixstaging/game/src/libltclientlogin/../../../runtime/src/libltmessaging/../libltcrypto/auth_internal.h:
+third_party/cryptopp890/base64.h:
+third_party/cryptopp890/cryptlib.h:
+third_party/cryptopp890/config.h:
+third_party/cryptopp890/config_align.h:
+third_party/cryptopp890/config_asm.h:
+third_party/cryptopp890/config_os.h:
+third_party/cryptopp890/config_ver.h:
+third_party/cryptopp890/config_cpu.h:
+third_party/cryptopp890/config_cxx.h:
+third_party/cryptopp890/config_dll.h:
+third_party/cryptopp890/config_int.h:
+third_party/cryptopp890/config_ns.h:
+third_party/cryptopp890/config_misc.h:
+third_party/cryptopp890/stdcpp.h:
+third_party/cryptopp890/trap.h:
+third_party/cryptopp890/basecode.h:
+third_party/cryptopp890/filters.h:
+third_party/cryptopp890/simple.h:
+third_party/cryptopp890/misc.h:
+third_party/cryptopp890/secblockfwd.h:
+third_party/cryptopp890/smartptr.h:
+third_party/cryptopp890/secblock.h:
+third_party/cryptopp890/allocate.h:
+third_party/cryptopp890/queue.h:
+third_party/cryptopp890/algparam.h:
+third_party/cryptopp890/integer.h:
+third_party/cryptopp890/argnames.h:
+third_party/cryptopp890/crc.h:
+third_party/cryptopp890/filters.h:
+third_party/cryptopp890/md5.h:
+third_party/cryptopp890/iterhash.h:
+third_party/cryptopp890/modes.h:
+third_party/cryptopp890/strciphr.h:
+third_party/cryptopp890/seckey.h:
+third_party/cryptopp890/osrng.h:
+third_party/cryptopp890/randpool.h:
+third_party/cryptopp890/aes.h:
+third_party/cryptopp890/rijndael.h:
+third_party/cryptopp890/fips140.h:
+third_party/cryptopp890/hkdf.h:
+third_party/cryptopp890/hmac.h:
+third_party/cryptopp890/rng.h:
+third_party/cryptopp890/sha.h:
+third_party/cryptopp890/oaep.h:
+third_party/cryptopp890/pubkey.h:
+third_party/cryptopp890/algebra.h:
+third_party/cryptopp890/modarith.h:
+third_party/cryptopp890/eprecomp.h:
+third_party/cryptopp890/rsa.h:
+third_party/cryptopp890/pkcspad.h:
+third_party/cryptopp890/hashfwd.h:
+third_party/cryptopp890/oaep.h:
+third_party/cryptopp890/emsa2.h:
+third_party/cryptopp890/asn.h:
+third_party/cryptopp890/sha.h:
+third_party/cryptopp890/twofish.h:
+matrixstaging/game/src/libltclientlogin/../../../runtime/src/libltmessaging/../liblttcp/ltthreadperclienttcpengine.h:
+matrixstaging/game/src/libltclientlogin/../../../runtime/src/libltmessaging/../liblttcp/ilttcpengine.h:
+matrixstaging/game/src/libltclientlogin/../../../runtime/src/libltmessaging/../liblttcp/lttcpconnection.h:
+matrixstaging/game/src/libltclientlogin/../../../runtime/src/liblttcp/ltipaddresslist.h:
+matrixstaging/game/src/libltclientlogin/loginmediator_base.h:
+matrixstaging/game/src/libltclientlogin/authbootstrap680.h:
+matrixstaging/game/src/libltclientlogin/loginmediator_state9_submit.h:
+matrixstaging/game/src/libltclientlogin/loginstate.h:
